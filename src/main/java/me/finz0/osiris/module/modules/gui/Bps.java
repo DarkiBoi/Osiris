64f3450fcd0e41138825751fbeb5b432523dcb58
FINZ0 <unconfigured@null.spigotmc.org>
2020-02-15 10:45:16 +0100

    bps component

--- src/main/java/me/finz0/osiris/module/modules/gui/Bps.java
+++ src/main/java/me/finz0/osiris/module/modules/gui/Bps.java
@@ -3,11 +3,6 @@ package me.finz0.osiris.module.modules.gui;
 import de.Hero.settings.Setting;
 import me.finz0.osiris.OsirisMod;
 import me.finz0.osiris.module.Module;
-import me.finz0.osiris.util.Rainbow;
-import net.minecraft.util.math.MathHelper;
-
-import java.awt.*;
-import java.text.DecimalFormat;
 
 public class Bps extends Module {
     public Bps() {
@@ -15,13 +10,11 @@ public class Bps extends Module {
         setDrawn(false);
     }
 
-    Setting red;
-    Setting green;
-    Setting blue;
-    Setting x;
-    Setting y;
-    Setting rainbow;
-    final DecimalFormat df = new DecimalFormat("0.0");
+    public Setting red;
+    public Setting green;
+    public Setting blue;
+    public Setting rainbow;
+    public Setting customFont;
 
     public void setup(){
         red = new Setting("bpsRed", this, 255, 0, 255, true);
@@ -30,25 +23,9 @@ public class Bps extends Module {
         OsirisMod.getInstance().settingsManager.rSetting(red);
         OsirisMod.getInstance().settingsManager.rSetting(green);
         OsirisMod.getInstance().settingsManager.rSetting(blue);
-        x = new Setting("bpsX", this, 2, 0, 1000, true);
-        y = new Setting("bpsY", this, 2, 0, 1000, true);
-        OsirisMod.getInstance().settingsManager.rSetting(x);
-        OsirisMod.getInstance().settingsManager.rSetting(y);
-        rainbow = new Setting("bpsRainbow", this, true);
+        rainbow = new Setting("bpsRainbow", this, false);
         OsirisMod.getInstance().settingsManager.rSetting(rainbow);
-    }
-
-    public void onRender(){
-        final double deltaX = mc.player.posX - mc.player.prevPosX;
-        final double deltaZ = mc.player.posZ - mc.player.prevPosZ;
-        final float tickRate = (mc.timer.tickLength / 1000.0f);
-        final String bps = df.format((MathHelper.sqrt(deltaX * deltaX + deltaZ * deltaZ) / tickRate)) + " BPS";
-        Color c = new Color((int)red.getValDouble(), (int)green.getValDouble(), (int)blue.getValDouble());
-        if(rainbow.getValBoolean()){
-            mc.fontRenderer.drawStringWithShadow(bps, (int) x.getValDouble(), (int) y.getValDouble(), Rainbow.getInt());
-        } else {
-            mc.fontRenderer.drawStringWithShadow(bps, (int) x.getValDouble(), (int) y.getValDouble(), c.getRGB());
-        }
+        OsirisMod.getInstance().settingsManager.rSetting(customFont = new Setting("bpsCFont", this, false));
     }
 
 
