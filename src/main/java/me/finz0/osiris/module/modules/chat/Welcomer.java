1dc1af4ae4f40d30a7468a919ef072a689cc54c5
FINZ0 <unconfigured@null.spigotmc.org>
2020-02-15 11:19:11 +0100

    welcomer public setting

--- src/main/java/me/finz0/osiris/module/modules/chat/Welcomer.java
+++ src/main/java/me/finz0/osiris/module/modules/chat/Welcomer.java
@@ -1,5 +1,6 @@
 package me.finz0.osiris.module.modules.chat;
 
+import de.Hero.settings.Setting;
 import me.finz0.osiris.OsirisMod;
 import me.finz0.osiris.command.Command;
 import me.finz0.osiris.event.events.PlayerJoinEvent;
@@ -12,16 +13,20 @@ import me.zero.alpine.listener.Listener;
 public class Welcomer extends Module {
     public Welcomer() {
         super("Welcomer", Category.CHAT);
+        OsirisMod.getInstance().settingsManager.rSetting(publicS = new Setting("wPublic", this, false));
     }
+    Setting publicS;
 
     @EventHandler
     private Listener<PlayerJoinEvent> listener1 = new Listener<>(event -> {
-        Command.sendClientMessage(event.getName() + " joined the game");
+        if(publicS.getValBoolean()) mc.player.sendChatMessage(event.getName() + " joined the game");
+        else Command.sendClientMessage(event.getName() + " joined the game");
     });
 
     @EventHandler
-    private Listener<PlayerLeaveEvent> listener2 = new Listener<>(event2 -> {
-        Command.sendClientMessage(event2.getName() + " left the game");
+    private Listener<PlayerLeaveEvent> listener2 = new Listener<>(event -> {
+        if(publicS.getValBoolean()) mc.player.sendChatMessage(event.getName() + " left the game");
+        else Command.sendClientMessage(event.getName() + " left the game");
     });
 
     public void onEnable(){
